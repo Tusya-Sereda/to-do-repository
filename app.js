fd3c57c79334249c5fd8fb50be8201496c6550f8
user <a.shevchenko.exceedteam@gmail.com>
2021-06-02 16:53:15 +0300

    Route for delete and edid task

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const { Schema } = mongoose; // через таблицы мы работаем 
 //создаём свою схему, как на клиентской стороне 
 const taskScheme = new Schema ({
     text: String,
-    isCheck: Boolean
+    isCheck: Boolean,
+    id: Number
 })
 
 const uri = "mongodb+srv://Natalia_25:nata2502@db-for-to-do.rwdqq.mongodb.net/DB-For-To-DO?retryWrites=true&w=majority";
@@ -18,46 +19,37 @@ mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindA
 const Task = mongoose.model("tasks", taskScheme);
 
 app.use(cors());
+app.use(express.json());
 
-// app.get('/allTasks', (req, res) => {
-//     Task.find().then(result => {
-//         res.send({data:result});
-//     });
-// });
-
-// app.post('/createTask', (req, res) => {
-//     const task = new Task(req.body);
-//     task.save().then(result => {
-//         res.send('Task');
-//     });
-// });
-
-app.get('/', (req, res) => { //get, post, patch, delete
-    const task = new Task({
-        text: "Second task",
-        isCheck: false
+app.get('/allTasks', (req, res) => { //получаем allTasks
+    Task.find().then(result => { //find указываем пустым, так как нужно получить все
+        res.send({data:result}); //когда получим наши таски отправляем их пользователю 
     });
+});
+
+app.post('/createTask', (req, res) => { // сохрняем задачу в БД
+    const task = new Task(req.body); // создаём локальную переменную Task и передаём туда req.body, которое хранить тело нашей задачи
     task.save().then(result => {
         res.send(result);
     }).catch(err => console.log(err));
 });
 
-app.get('/paramRequest', (req, res) => {
-    Task.find().then(result => {
-        res.send({data: result}); //возвращаем данные на сервер 
+app.patch('/updateTask', (req, res) => { //обновление данных  
+    Task.updateOne({_id: req.body._id}, req.body).then(result => {
+        console.log(req.body)
+        Task.find({_id: req.body._id}).then(result => {
+            res.send({data: result});
+        })
     })
-});
-
-app.post('/post/user/:id', (req, res) => {
-    res.send('post /');
+    res.send('Success patch /');
 })
 
-app.put('/', (req, res) => { //обновление данных 
-    res.send('put /');
-})
-
-app.delete('/', (req, res) => {
-    res.send('delete /');
+app.delete('/deleteTask', (req, res) => { //как get запрос: присылаем body и id элементов
+    Task.deleteOne({_id: req.body._id}).then(result => {
+        Task.find().then (result => {
+        res.send({data: result});  
+        })
+    })
 })
 
 app.listen(8000, () => {
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let allTasks = []; // получение//(парсим) наши таски, если данных нет, то массив пустой
 let valueInput = '';
 let input = null; // всё время обнулять input
-let indexEdit = -1;
+let indexEdit = null;
 
 window.onload = async function init () {
   input = document.getElementById('add-task'); //с input получаем элемент по id - находим в документе
@@ -14,30 +14,19 @@ window.onload = async function init () {
   });
   
   let resultMethod = await response.json(); // переводим данные в json формат
-  console.log('result', resultMethod);
+  // console.log('result', resultMethod);
   // перезаписываем в наш массив данные полученные с сервера
   allTasks = resultMethod.data; // data так как на сервере этот масив именуется как data 
-  
-  render(); // если элементы есть прям сразу, то после того, как загрузится наша страница. Можно сразу перейти к рендеру и отобразить все наши таски 
+
+  render(); 
+  // если элементы есть прям сразу, то после того, как загрузится наша страница. Можно сразу перейти к рендеру и отобразить все наши таски 
 }
 
 onClickButton = async () => { // при нажатии на клик 
   
   if(valueInput){
-    
-    allTasks.push({
-      text: valueInput, // текст, который мы получаем из input
-      isCheck: false // состояние 
-    })
-    
-    //после каждого нажатия кнопки Add запускается функция render(), которая будет записывать наши таски
-    render(); //будет отображать все наши элементы
-  }else{
-    alert('Неа, так незя делать');
-  }
-
-  // получаем данные с сервера с помощью async-await 
-  const response = await fetch('http://localhost:8000/createTask',{
+    // получаем данные с сервера с помощью async-await 
+    const response = await fetch('http://localhost:8000/createTask',{
     method: 'POST', // меняем метод на POST, то есть, добавление задачи на сервер
     headers: { //так как мы отправляем данные на удалённый сервер и так как два сервера локальных, нам необходимо их обрабатывать
       'Content-Type': 'application/json; charset=utf-8', //формат, в котором отдаём/принимаем значения формат: json, символы в формате utf-8
@@ -50,18 +39,26 @@ onClickButton = async () => { // при нажатии на клик
   });
   
   let resultMethod = await response.json(); // переводим данные в json формат
-  console.log('result', resultMethod);
+  // console.log('result', resultMethod);
   // перезаписываем в наш массив данные полученные с сервера
-  allTasks = resultMethod.data;
-  
+  // console.log(resultMethod)
+  allTasks.push(resultMethod);
+
   valueInput =''; // обнуляем значение инпута
   input.value = ''; // присваиваем значение валью инпуту - пустую строку, после записи в массив, инпут и валью обнуляются
 
+  //после каждого нажатия кнопки Add запускается функция render(), которая будет записывать наши таски
+  render(); //будет отображать все наши элементы
+
+  }else{
+    alert('Неа, так незя делать');
+    return;
+  }
 }
 
 updateValue = (event) => { // в функцию получаем какое-то событие
   valueInput = event.target.value; // присваиваем valueInput значения с нашего инпута
-  console.log('value', valueInput);
+  // console.log('value', valueInput);
 }
 
 /*При нажатии на ENTER*/
@@ -156,7 +153,7 @@ render = () => {
     content.appendChild(container);
   });
 
-  console.log(allTasks);
+  // console.log(allTasks);
 }
 
 onChangeCheckBox = (index) => {
@@ -167,24 +164,24 @@ onChangeCheckBox = (index) => {
 onClickImageEdit = (index) => {
   indexEdit = index;
   render();
-  
-}
+};
 
 onClickImageDelete = async (index) => {
-  // allTasks.splice(index, 1);
-
   // получаем данные с сервера с помощью async-await 
-  const response = await fetch(`http://localhost:8000/deleteTask?id=${allTasks[index].id}`,{
+  const response = await fetch(`http://localhost:8000/deleteTask`,{
     method: 'DELETE', // только получаем данные 
     headers: { //так как мы отправляем данные на удалённый сервер и так как два сервера локальных, нам необходимо их обрабатывать
       'Content-Type': 'application/json; charset=utf-8', //формат, в котором отдаём/принимаем значения формат: json, символы в формате utf-8
       'Access-Control-Allow-Origin': '*'//разрешение на наше локальное приложение
     },
+    body: JSON.stringify({ //основное о нашем таске
+      _id: allTasks[index]._id
+    })
   });
   
   let resultMethod = await response.json(); // переводим данные в json формат
   // resultMethod.splice(index, 1);
-  console.log('result', resultMethod);
+  // console.log('result', resultMethod);
   // перезаписываем в наш массив данные полученные с сервера
   allTasks = resultMethod.data; // data так как на сервере этот масив именуется как data 
   render(); // запускаю свою же функцию, которая удаляет елемент 
@@ -196,27 +193,26 @@ onClickImageBack = (index) => {
 }
 
 onClickImageCheck = async (digit, index) => {
-  // allTasks[index].text = digit;
   indexEdit = null;
-
   //получаем данные с сервера 
+  
   const response = await fetch('http://localhost:8000/updateTask',{
     method: 'PATCH', // меняем метод на POST, то есть, добавление задачи на сервер
     headers: { //так как мы отправляем данные на удалённый сервер и так как два сервера локальных, нам необходимо их обрабатывать
       'Content-Type': 'application/json; charset=utf-8', //формат, в котором отдаём/принимаем значения формат: json, символы в формате utf-8
       'Access-Control-Allow-Origin': '*'//разрешение на наше локальное приложение
     },
-    body: JSON.stringify({ //основное о нашем таске
+    body: JSON.stringify({ 
       text: digit,
-      isCheck: false,
-      id: allTasks[index].id
+      isCheck: allTasks[index].isCheck,
+      _id: allTasks[index]._id
     })
   });
-  
-  let resultMethod = await response.json(); // переводим данные в json формат
-  console.log('result', resultMethod);
-  // перезаписываем в наш массив данные полученные с сервера
-  allTasks = resultMethod.data;
+
+  // let resultMethod = await response.json(); // переводим данные в json формат
+  allTasks[index].text = digit;
+  console.log(allTasks[index].data)
+  indexEdit = null;
 
   render();
 }
